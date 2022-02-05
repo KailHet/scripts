767fc6e217bd7f3a5343a6f961815133eb0ed3d8
KailHet <71993307+KailHet@users.noreply.github.com>
2022-02-05 21:53:52 +0500

    Create discord_activities.js
    Just Discord Activities for Voice Channels :)

---
+++ discord_activities.js
@@ -0,0 +1,38 @@
+// Activity IDs
+// Poker Night 755827207812677713 
+// Chess In The Park 832012774040141894
+// Checkers In The Park 832013003968348200
+// Doodle Crew 878067389634314250
+// Word Snacks 879863976006127627
+// Letter League 879863686565621790
+// Spell Cast 852509694341283871
+// Watch Together 880218394199220334 
+// Betrayal.io 773336526917861400 
+// Fishington.io 814288819477020702 
+// Awkword 879863881349087252 
+// Ocho 832025144389533716
+// Sketch Heads 902271654783242291
+
+const fetch = require('node-fetch');
+const game = tempVars('game');
+const channel = tempVars('channel_id')
+
+fetch(`https://discord.com/api/v8/channels/${channel}/invites`, {
+ method: "POST",
+ body: JSON.stringify({
+  max_age: 86400,
+  max_uses: 0,
+  target_application_id: game,
+  target_type: 2,
+  temporary: false,
+  validate: null
+ }),
+ headers: {
+  "Authorization": `Bot ${client.token}`,
+  "Content-Type": "application/json"
+ }
+})
+.then(res => res.json())
+.then(invite => {
+ interaction.channel.send(`Ссылка на игру: https://discord.gg/${invite.code}`)
+});

3d174ffc6fe7974227b50ad4826d60a7599b981e
Ernest <71993307+KailHet@users.noreply.github.com>
2024-11-05 14:31:11 +0500

    translated to english

--- replace_from_arr.js
+++ replace_from_arr.js
@@ -1,25 +1,28 @@
 // Строка которую надо изменить
-let str = 'Города России: Москва, Питер, Новосибирск' 
+// String that we need to change
+let str = 'Cities of Russia: Moscow, Saint-Petersburg, Novosibirsk' 
 
 // Массив с элементами, которые мы заменяем
+// Array with elements that we need to change
 const cities = [ 
-    "Новосибирск", 
-    "Екатеринбург", 
-    "Самара", 
-    "Москва", 
-    "Санкт-Петербург", 
-    "Краснодар", 
-    "Ярославль", 
-    "Казань", 
-    "Челябинск", 
-    "Омск", 
-    "Ростов на Дону",
-    "Уфа", 
-    "Нижний Новгород", 
-    "Волгоград"
+    "Yekaterinburg", 
+    "Samara", 
+    "Moscow", 
+    "Saint-Petersburg", 
+    "Krasnodar", 
+    "Yaroslavl", 
+    "Kazan", 
+    "Chelyabinsk", 
+    "Omsk", 
+    "Rostov on Don",
+    "Ufa", 
+    "Volgograd"
 ]
 
 // Создаем regexp: /(элемент1|элемент2|и тд)/g
+// creating regexp /(element1|element2|etc)/g
 let re = new RegExp('('+cities.join('|')+')', 'g')
 
-console.log(str.replace(re, 'тест'))
+console.log(str.replace(re, '***'))
+// Output:
+// Cities of Russia: ***, ***, Novosibirsk
